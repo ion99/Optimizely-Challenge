4fad35356d3ca7db2ac4e526ec84bb33f3d87c16
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-07 16:48:20 -0700

    Use freegeo.ip for location, toUpperCase() for city name, css = clock class to scale the flipclock

--- views/javascript/geoLoc.js
+++ views/javascript/geoLoc.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     
-  $.getJSON("https://api.wunderground.com/api/c451ff398a91d3b1/forecast/conditions/q/autoip.json", function(data){
-    console.log("Yahooo", data.current_observation.display_location.full);
-    $(".modal-title").append("<h2><b>WAIT! SPECIAL OFFER FOR " +  "<span class='city'>" + data.current_observation.display_location.full + "</span>" + " RESIDENTS...</b></h2><h1><b>Claim your instant discount before the timer runs out!</b></h1>") 
+  // $.getJSON("https://api.wunderground.com/api/c451ff398a91d3b1/forecast/conditions/q/autoip.json", function(data){
+  //   console.log("Yahooo", data.current_observation.display_location.city);
+  //   // console.log("Yahooo", data.current_observation);
+  //   $(".modal-title").append("<h2><b>WAIT! SPECIAL OFFER FOR " +  "<span class='city'>" + data.current_observation.display_location.city.toUpperCase() + "</span>" + " RESIDENTS...</b></h2><br><img id='content' src='imges/redArrow.png'><h1><b>Claim your instant discount before the timer runs out!</b></h1>") 
+  // })
+  $.getJSON("https://freegeoip.net/json/", function(data){
+    console.log("Buhooo", data.city);
+    // console.log("Yahooo", data.current_observation);
+    $(".modal-title").append("<br><h2><b>WAIT! SPECIAL OFFER FOR " +  "<span class='city'>" + data.city.toUpperCase() + "</span>" + " RESIDENTS...</b></h2><br><img id='content' src='imges/redArrow.png'><h1><b>Claim your instant discount before the timer runs out!</b></h1>") 
   })
 
 });
