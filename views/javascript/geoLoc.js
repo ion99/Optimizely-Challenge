be521f8a3a75e54a377d1829963adb457111259c
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-07 15:21:29 -0700

    Added an image, update the layout, and view

--- views/javascript/geoLoc.js
+++ views/javascript/geoLoc.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
     console.log("Yahooo", data.current_observation.display_location.full);
     $(".modal-title").append("<h2><b>WAIT! SPECIAL OFFER FOR " +  "<span class='city'>" + data.current_observation.display_location.full + "</span>" + " RESIDENTS...</b></h2><h1><b>Claim your instant discount before the timer runs out!</b></h1>") 
   })
-  
+
 });
